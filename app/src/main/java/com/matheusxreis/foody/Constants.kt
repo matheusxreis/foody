ed73da60d2c8ec7ff15afa9289fe08e5267afe56
Matheus Reis <matheus.reis081@gmail.com>
2023-01-20 22:24:18 -0300

    feat: created Constants class

---
+++ app/src/main/java/com/matheusxreis/foody/Constants.kt
@@ -0,0 +1,10 @@
+package com.matheusxreis.foody
+
+class Constants {
+
+    companion object {
+        const val API_KEY=BuildConfig.apiKey
+        const val BASE_URL="https://api.spoonacular.com"
+    }
+
+}
