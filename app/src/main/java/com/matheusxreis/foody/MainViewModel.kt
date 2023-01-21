e9131c377d4c643ba1ccca26befa1eb277d165fb
Matheus Reis <matheus.reis081@gmail.com>
2023-01-21 14:10:01 -0300

    feat: created main view model and inject repository

---
+++ app/src/main/java/com/matheusxreis/foody/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.matheusxreis.foody
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.AndroidViewModel
+import com.matheusxreis.foody.data.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: Repository,
+    application: Application):AndroidViewModel(application){
+
+}
