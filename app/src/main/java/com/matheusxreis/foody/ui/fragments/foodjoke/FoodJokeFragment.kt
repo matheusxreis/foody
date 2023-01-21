7bacfd6390888d88627a53e7a151c0970d056b3c
Matheus Reis <matheus.reis081@gmail.com>
2023-01-20 22:29:46 -0300

    refactor: create ui layer with fragments directory and move main activity and fragments

--- app/src/main/java/com/matheusxreis/foody/ui/fragments/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/matheusxreis/foody/ui/fragments/foodjoke/FoodJokeFragment.kt
@@ -1,24 +1,21 @@
-package com.matheusxreis.foody
+package com.matheusxreis.foody.ui.fragments.foodjoke
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_recipes.view.*
+import com.matheusxreis.foody.R
 
-class RecipesFragment : Fragment() {
+
+class FoodJokeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
-
-        view.shimmer_recycler_view.showShimmer()
-
-        return view
+        return inflater.inflate(R.layout.fragment_food_joke, container, false)
     }
 
 }
