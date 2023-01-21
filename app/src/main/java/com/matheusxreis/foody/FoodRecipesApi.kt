78a7c63a3a9f3d4ca785ba66cae7c5fc408e9640
Matheus Reis <matheus.reis081@gmail.com>
2023-01-20 22:38:02 -0300

    feat: created FoodRecipesApi interface

---
+++ app/src/main/java/com/matheusxreis/foody/FoodRecipesApi.kt
@@ -0,0 +1,14 @@
+package com.matheusxreis.foody
+
+import com.matheusxreis.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap queries: Map<String, String> // -> @QueryMap allows that I define my queries inside a Map when call the function
+    ):Response<FoodRecipe>
+}
