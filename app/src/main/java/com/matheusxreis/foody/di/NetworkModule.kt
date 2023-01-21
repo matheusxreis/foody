1ea53d472e27bae5503d4559351dfcaa23f16a18
Matheus Reis <matheus.reis081@gmail.com>
2023-01-21 13:34:33 -0300

    feat: created NetworkModule in di module

---
+++ app/src/main/java/com/matheusxreis/foody/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.matheusxreis.foody.di
+
+import com.matheusxreis.foody.Constants
+import com.matheusxreis.foody.FoodRecipesApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+// -> InstallIn is used to define which scope I want this be provided
+// -> @Singleton and another scope annotations is used to define the lifecycle of my class,
+// in case of @Singleton, my class lifecycle is with my Application
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient():OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory():GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ):Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit):FoodRecipesApi {
+        return retrofit.create(FoodRecipesApi::class.java)
+    }
+}
