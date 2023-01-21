dc0986e3bac3a9759242d7eeae01438ebccc415b
Matheus Reis <matheus.reis081@gmail.com>
2023-01-21 13:47:09 -0300

    feat: inject food recipes api in remote data source

--- app/src/main/java/com/matheusxreis/foody/RemoteDataSource.kt
+++ app/src/main/java/com/matheusxreis/foody/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.matheusxreis.foody
 
-class RemoteDataSource() {
+import com.matheusxreis.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+)  {
+
+    suspend fun getRecipes(queries:Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
