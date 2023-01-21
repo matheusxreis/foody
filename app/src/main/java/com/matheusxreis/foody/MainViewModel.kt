5da2f48e5fa230074cbbf66174f4acd9e3fd9947
Matheus Reis <matheus.reis081@gmail.com>
2023-01-21 14:12:13 -0300

    feat: created function to verify if has internt connection in main view model

--- app/src/main/java/com/matheusxreis/foody/MainViewModel.kt
+++ app/src/main/java/com/matheusxreis/foody/MainViewModel.kt
@@ -16,4 +16,21 @@ class MainViewModel @Inject constructor(
     private val repository: Repository,
     application: Application):AndroidViewModel(application){
 
+    private fun hasInternetConnection():Boolean {
+        val connectivityManager = getApplication<Application>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        val activityNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activityNetwork) ?: return false
+
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+
+
 }
