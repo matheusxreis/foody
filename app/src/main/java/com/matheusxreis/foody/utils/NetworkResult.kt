1b007131312b5ba52af03bbc3b49dd241e050be2
Matheus Reis <matheus.reis081@gmail.com>
2023-01-21 14:01:09 -0300

    feat: created network result sealed class

---
+++ app/src/main/java/com/matheusxreis/foody/utils/NetworkResult.kt
@@ -0,0 +1,10 @@
+package com.matheusxreis.foody.utils
+
+sealed class NetworkResult<T>(
+    val data:T? = null,
+    val message:String? = null
+){
+    class Success<T>(data: T):NetworkResult<T>(data)
+    class Error<T>(message:String?, data:T?): NetworkResult<T>(data, message)
+    class Loading<T>:NetworkResult<T>()
+}
