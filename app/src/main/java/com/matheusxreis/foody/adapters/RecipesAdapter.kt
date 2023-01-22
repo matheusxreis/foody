1f80094f9d6684cfa76387f77a1029d5aa752870
Matheus Reis <matheus.reis081@gmail.com>
2023-01-22 10:56:01 -0300

    feat: implements recipes adapter methods and created setData method

--- app/src/main/java/com/matheusxreis/foody/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/matheusxreis/foody/adapters/RecipesAdapter.kt
@@ -4,22 +4,28 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.matheusxreis.foody.databinding.RecipesRowLayoutBinding
+import com.matheusxreis.foody.models.FoodRecipe
 import com.matheusxreis.foody.models.Result
 
+// adapter is a structural design pattern that allows objects with incompatible interfaces to collaborate
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
 
     private var recipe = emptyList<Result>()
 
+    // this class is the layout which each item of recycler view
     class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-            fun bind(result:Result) {
-                binding.result = result
-                binding.executePendingBindings()
-                // ^ this function update the layout when is a change in the data
-            }
-        companion object  {
-            fun from(parent:ViewGroup):MyViewHolder{
+        fun bind(result: Result) {
+            // with this method, I bind the result variable of my xml layout to the actual item
+            // that the user is interacting
+            binding.result = result
+            binding.executePendingBindings()
+            // ^ this function update the layout when is a change in the data
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
                 return MyViewHolder(binding)
@@ -28,15 +34,23 @@ class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
         }
     }
 
+    // this method must return the creation of my viewl holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder.from(parent)
     }
 
+    // this method must pass to my view holder, the actual item which the user is interacting
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentResult = recipe[position]
+        holder.bind(currentResult)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    // this method must return the amoutn of items in my rv
+    override fun getItemCount(): Int = recipe.size
+
+    // this method created by me, must populate the recycler view
+    fun setData(newData: FoodRecipe) {
+        recipe = newData.results
+        notifyDataSetChanged()
     }
 }
