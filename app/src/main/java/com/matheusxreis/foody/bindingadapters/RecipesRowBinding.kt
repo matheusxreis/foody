0b1f5f9ed749e48454df4bf90ae80a46f3186b98
Matheus Reis <matheus.reis081@gmail.com>
2023-01-22 22:21:01 -0300

    feat: created recipes row binding adapter and the first function, setNumberOfLikes

---
+++ app/src/main/java/com/matheusxreis/foody/bindingadapters/RecipesRowBinding.kt
@@ -0,0 +1,20 @@
+package com.matheusxreis.foody.bindingadapters
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+class RecipesRowBinding {
+
+    companion object {
+
+        @BindingAdapter("setNumberOfLikes")
+        @JvmStatic
+        fun setNumberOfLikes(
+            textView: TextView,
+            likes: Int
+        ) {
+            textView.text = likes.toString()
+        }
+    }
+
+}
