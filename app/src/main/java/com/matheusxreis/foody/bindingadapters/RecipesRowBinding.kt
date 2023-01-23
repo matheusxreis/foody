14ae3770a312e12aa867422576ae0c70ca7c9021
Matheus Reis <matheus.reis081@gmail.com>
2023-01-22 22:40:31 -0300

    feat: created binding adapter to load image from url

--- app/src/main/java/com/matheusxreis/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/matheusxreis/foody/bindingadapters/RecipesRowBinding.kt
@@ -5,12 +5,23 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import coil.load
 import com.matheusxreis.foody.R
 
 class RecipesRowBinding {
 
     companion object {
 
+        @BindingAdapter("loadImageFromUrl")
+        @JvmStatic
+        fun loadImageFromUrl(
+            imageView: ImageView,
+            imageUrl:String
+        ){
+            imageView.load(imageUrl) {
+                crossfade(600)
+            }
+        }
         @BindingAdapter("setNumberOfLikes")
         @JvmStatic
         fun setNumberOfLikes(
