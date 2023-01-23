c1828b016505b54fd95c34bd7e83fe63b4d866dd
Matheus Reis <matheus.reis081@gmail.com>
2023-01-23 09:14:21 -0300

    feat: added query constants in utils package

--- app/src/main/java/com/matheusxreis/foody/utils/Constants.kt
+++ app/src/main/java/com/matheusxreis/foody/utils/Constants.kt
@@ -7,6 +7,15 @@ class Constants {
     companion object {
         const val API_KEY= BuildConfig.apiKey
         const val BASE_URL="https://api.spoonacular.com"
+
+        // API Query keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENT = "fillIngredient"
+
     }
 
 }
