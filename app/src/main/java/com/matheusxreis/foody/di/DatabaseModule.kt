ef5516dfcbb3f3b6ed1c7c5506d10c31469c9707
Matheus Reis <matheus.reis081@gmail.com>
2023-01-23 19:26:25 -0300

    feat: created database module in di layer

---
+++ app/src/main/java/com/matheusxreis/foody/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.matheusxreis.foody.di
+
+import android.content.Context
+import androidx.room.Room
+import com.matheusxreis.foody.data.database.RecipesDatabase
+import com.matheusxreis.foody.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        Constants.DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(
+        database:RecipesDatabase
+    ) = database.recipesDao()
+}
