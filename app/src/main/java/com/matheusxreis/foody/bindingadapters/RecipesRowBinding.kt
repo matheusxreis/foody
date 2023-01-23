e08c5b44e635d4fe6997f07ba6cf5e4fd544f3aa
Matheus Reis <matheus.reis081@gmail.com>
2023-01-22 22:34:26 -0300

    feat: created binding adapter to number of minutes and applyvegancolor

--- app/src/main/java/com/matheusxreis/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/matheusxreis/foody/bindingadapters/RecipesRowBinding.kt
@@ -1,7 +1,11 @@
 package com.matheusxreis.foody.bindingadapters
 
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.matheusxreis.foody.R
 
 class RecipesRowBinding {
 
@@ -14,6 +18,43 @@ class RecipesRowBinding {
             likes: Int
         ) {
             textView.text = likes.toString()
+        }
+
+        @BindingAdapter("setNumberOfMinutes")
+        @JvmStatic
+        fun setNumberOfMinutes(
+            textView: TextView,
+            minutes:Int
+        ){
+            textView.text = minutes.toString()
+        }
+
+        @BindingAdapter("applyVeganColor")
+        @JvmStatic
+        fun applyVeganColor(
+            view:View,
+            vegan:Boolean
+        ){
+            if(vegan){
+                when(view){
+                    is TextView -> {
+                        view.setTextColor(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                    is ImageView -> {
+                        view.setColorFilter(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                }
+            }
         }
     }
 
