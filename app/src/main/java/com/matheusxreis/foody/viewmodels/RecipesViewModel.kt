5d18428b25a2375a138e29b0f0a1456764807643
Matheus Reis <matheus.reis081@gmail.com>
2023-01-24 17:05:11 -0300

    feat: inject datastore repository in recipe vm and created functions to call and read it

--- app/src/main/java/com/matheusxreis/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/matheusxreis/foody/viewmodels/RecipesViewModel.kt
@@ -2,14 +2,41 @@ package com.matheusxreis.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.matheusxreis.foody.BuildConfig
+import com.matheusxreis.foody.data.DataStoreRepository
 import com.matheusxreis.foody.utils.Constants
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application):AndroidViewModel(application){
+@HiltViewModel
+class RecipesViewModel @Inject constructor(application: Application, private val dataStoreRepository:DataStoreRepository):AndroidViewModel(application){
 
 
+    private var mealType = Constants.DEFAULT_MEAL_TYPE
+    private var dietType = Constants.DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType:String, mealTypeId:Int, dietType: String, dietTypeId:Int) = viewModelScope.launch(Dispatchers.IO){
+        dataStoreRepository.saveMealAndDietType(
+            mealType = mealType,
+            mealTypeId = mealTypeId,
+            dietType = dietType,
+            dietTypeId = dietTypeId
+        )
+    }
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
+
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
 
         queries[Constants.QUERY_NUMBER] =  Constants.DEFAULT_RECIPES_NUMBER
         queries[Constants.QUERY_API_KEY] = BuildConfig.apiKey
