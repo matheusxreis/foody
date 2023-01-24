3a247c4645d087848e17ee01d961e1e971b23e98
Matheus Reis <matheus.reis081@gmail.com>
2023-01-24 17:10:06 -0300

    refactor: change queries value to datastore persisted value

--- app/src/main/java/com/matheusxreis/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/matheusxreis/foody/viewmodels/RecipesViewModel.kt
@@ -40,8 +40,8 @@ class RecipesViewModel @Inject constructor(application: Application, private val
 
         queries[Constants.QUERY_NUMBER] =  Constants.DEFAULT_RECIPES_NUMBER
         queries[Constants.QUERY_API_KEY] = BuildConfig.apiKey
-        queries[Constants.QUERY_TYPE] = Constants.DEFAULT_MEAL_TYPE
-        queries[Constants.QUERY_DIET] = Constants.DEFAULT_DIET_TYPE
+        queries[Constants.QUERY_TYPE] = mealType
+        queries[Constants.QUERY_DIET] = dietType
         queries[Constants.QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[Constants.QUERY_FILL_INGREDIENT] = "true"
 
