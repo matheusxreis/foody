0b4803a111f1deb53818b848b329fbcf806a5627
Matheus Reis <matheus.reis081@gmail.com>
2023-01-24 10:20:32 -0300

    refactor: rename bottom sheet layout

--- app/src/main/java/com/matheusxreis/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/matheusxreis/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -1,7 +1,6 @@
 package com.matheusxreis.foody.ui.fragments.recipes.bottomsheet
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +15,7 @@ class RecipesBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes_bottom_sheet, container, false)
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
     }
 
 }
