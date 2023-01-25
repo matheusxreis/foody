8f1afa596150354d08afdee75d17725c60475546
Matheus Reis <matheus.reis081@gmail.com>
2023-01-25 01:15:02 -0300

    feat: added logic to call api again in case of coming back from bottom sheet

--- app/src/main/java/com/matheusxreis/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/matheusxreis/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.matheusxreis.foody.R
 import com.matheusxreis.foody.adapters.RecipesAdapter
@@ -22,6 +23,8 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private val args by navArgs<RecipesFragmentArgs>()
 
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
@@ -49,7 +52,7 @@ class RecipesFragment : Fragment() {
         binding.mainViewModel = mainViewModel //variable in xml
 
         setupRecyclerView()
-         readDatabase()
+        readDatabase()
 
         binding.fab.setOnClickListener {
             findNavController().navigate(
@@ -73,7 +76,7 @@ class RecipesFragment : Fragment() {
     private fun readDatabase() {
         lifecycleScope.launch {
             mainViewModel.readRecipes.observeOnce(viewLifecycleOwner) { database ->
-                if(database.isNotEmpty()){
+                if(database.isNotEmpty() && !args.backFromBottomSheet){
                     mAdapter.setData(database[0].foodRecipe)
                     hideShimmerEffect()
                 }else {
