0008542616e09d67a747fcdb71c20aee251ead94
Matheus Reis <matheus.reis081@gmail.com>
2023-01-28 12:11:32 -0300

    feat: annoted result model to use as parcelable safeargs argument

--- app/src/main/java/com/matheusxreis/foody/models/Result.kt
+++ app/src/main/java/com/matheusxreis/foody/models/Result.kt
@@ -1,9 +1,13 @@
 package com.matheusxreis.foody.models
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import com.matheusxreis.foody.models.ExtendedIngredient
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
+@Parcelize
 data class Result(
     @SerializedName("aggregateLikes")
     val aggregateLikes: Int,
@@ -12,7 +16,7 @@ data class Result(
     @SerializedName("dairyFree")
     val dairyFree: Boolean,
     @SerializedName("extendedIngredients")
-    val extendedIngredients: List<ExtendedIngredient>,
+    val extendedIngredients: @RawValue List<ExtendedIngredient>,
     @SerializedName("glutenFree")
     val glutenFree: Boolean,
     @SerializedName("id")
@@ -39,4 +43,4 @@ data class Result(
     val vegetarian: Boolean,
     @SerializedName("veryHealthy")
     val veryHealthy: Boolean,
- )
+ ):Parcelable
