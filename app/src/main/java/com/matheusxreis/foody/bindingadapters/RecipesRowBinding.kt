e498d1442d24f5428f9c685b283fd85a4713dd9c
Matheus Reis <matheus.reis081@gmail.com>
2023-01-28 12:26:32 -0300

    feat: created binding adapter to listener click in row layout and go to details activity

--- app/src/main/java/com/matheusxreis/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/matheusxreis/foody/bindingadapters/RecipesRowBinding.kt
@@ -1,16 +1,36 @@
 package com.matheusxreis.foody.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.matheusxreis.foody.R
+import com.matheusxreis.foody.models.Result
+import com.matheusxreis.foody.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesRowBinding {
 
     companion object {
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout:ConstraintLayout, result:Result){
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action = RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(
+                        result = result
+                    )
+                    recipeRowLayout.findNavController().navigate(action)
+                }catch(exception:Exception){
+                    Log.d("onRecipeClickListener", exception.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
