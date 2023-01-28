2c7c26c9ec96e0b9d83fd1efdc2ec04d8c99f3af
Matheus Reis <matheus.reis081@gmail.com>
2023-01-28 13:05:00 -0300

    feat: set fragments in view pager in details activity
    define the pager adapter in view pager 2 and used tab layout mediator to pass correct titles to fragments

--- app/src/main/java/com/matheusxreis/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/matheusxreis/foody/ui/DetailsActivity.kt
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.navArgs
+import com.google.android.material.tabs.TabLayoutMediator
 import com.matheusxreis.foody.R
+import com.matheusxreis.foody.adapters.PagerAdapter
+import com.matheusxreis.foody.ui.fragments.ingredients.IngredientsFragment
+import com.matheusxreis.foody.ui.fragments.instructions.InstructionsFragment
+import com.matheusxreis.foody.ui.fragments.overview.OverviewFragment
 import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
@@ -16,7 +27,33 @@ class DetailsActivity : AppCompatActivity() {
         toolbar.setTitleTextColor(
             ContextCompat.getColor(this, R.color.white)
         )
-        supportActionBar?.setDisplayHomeAsUpEnabled(true )
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val adapter = PagerAdapter(
+            resultBundle = resultBundle,
+            fragments = fragments,
+            titles = titles,
+            activity = this
+        )
+
+        view_pager.adapter = adapter
+        TabLayoutMediator(tab_layout, view_pager) {
+            tab, position -> tab.text = titles[position]
+        }.attach()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
