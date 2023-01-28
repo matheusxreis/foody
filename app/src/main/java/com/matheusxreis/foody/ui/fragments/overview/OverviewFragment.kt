1d924bfabe520112891b0bdc31fd526e5deef053
Matheus Reis <matheus.reis081@gmail.com>
2023-01-28 12:40:34 -0300

    feat: created three details fragments: overview, instructions and ingredients

---
+++ app/src/main/java/com/matheusxreis/foody/ui/fragments/overview/OverviewFragment.kt
@@ -0,0 +1,25 @@
+package com.matheusxreis.foody.ui.fragments.overview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.matheusxreis.foody.R
+
+class OverviewFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false)
+    }
+
+}
