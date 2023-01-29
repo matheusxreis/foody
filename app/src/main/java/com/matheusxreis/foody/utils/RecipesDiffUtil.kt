7e98ab0282fb3dd40f26e5fa0d916cac4c2dd54a
Matheus Reis <matheus.reis081@gmail.com>
2023-01-29 00:07:31 -0300

    refactor: done recipesdiffutil class deal with generic type instead of result type

--- app/src/main/java/com/matheusxreis/foody/utils/RecipesDiffUtil.kt
+++ app/src/main/java/com/matheusxreis/foody/utils/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.matheusxreis.foody.utils
 import androidx.recyclerview.widget.DiffUtil
 import com.matheusxreis.foody.models.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ) : DiffUtil.Callback() {
     override fun getOldListSize(): Int = oldList.size
 
