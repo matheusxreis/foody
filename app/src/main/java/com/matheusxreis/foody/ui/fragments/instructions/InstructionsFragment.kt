5f1e9397746127773f12fd63112e5f7e11a3ba54
Matheus Reis <matheus.reis081@gmail.com>
2023-01-29 01:37:34 -0300

    feat: design and finish fragment instructions

--- app/src/main/java/com/matheusxreis/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/matheusxreis/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.matheusxreis.foody.R
-
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
+import com.matheusxreis.foody.models.Result
+import com.matheusxreis.foody.utils.Constants
 
 class InstructionsFragment : Fragment() {
 
@@ -19,6 +22,15 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val mView = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        mView.instructions_web_view.webViewClient = object: WebViewClient() {}
+        val webUrl:String = myBundle!!.sourceUrl
+        mView.instructions_web_view.loadUrl(webUrl)
+
+        return mView
     }
 }
