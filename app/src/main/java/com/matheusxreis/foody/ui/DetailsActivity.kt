4fff2c52c9188eb591fe3a3e3a1b60c28376c007
Matheus Reis <matheus.reis081@gmail.com>
2023-01-29 13:35:51 -0300

    feat: created function to save in favorites in detaisl activity

--- app/src/main/java/com/matheusxreis/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/matheusxreis/foody/ui/DetailsActivity.kt
@@ -5,23 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 import com.matheusxreis.foody.R
 import com.matheusxreis.foody.adapters.PagerAdapter
+import com.matheusxreis.foody.data.database.entities.FavoritesEntity
 import com.matheusxreis.foody.ui.fragments.ingredients.IngredientsFragment
 import com.matheusxreis.foody.ui.fragments.instructions.InstructionsFragment
 import com.matheusxreis.foody.ui.fragments.overview.OverviewFragment
 import com.matheusxreis.foody.utils.Constants
+import com.matheusxreis.foody.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_details.*
 
+@AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
 
     private val args by navArgs<DetailsActivityArgs>()
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,26 +64,57 @@ class DetailsActivity : AppCompatActivity() {
         view_pager.adapter = adapter
         // the tab layout is just to view fragments title
         // who is responsable by slide between the fragments is the view pager2
-        TabLayoutMediator(tab_layout, view_pager) {
-            tab, position -> tab.text = titles[position]
+        TabLayoutMediator(tab_layout, view_pager) { tab, position ->
+            tab.text = titles[position]
         }.attach()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if(item.itemId === android.R.id.home){
+        if (item.itemId === android.R.id.home) {
+            //coming back
             finish()
+        } else if (item.itemId === R.id.save_to_favorite_menu) {
+            saveToFavorites(item)
         }
         return super.onOptionsItemSelected(item)
 
+    }
+
+    private fun saveToFavorites(item: MenuItem) {
+        val favoriteEntity = FavoritesEntity(
+            0,
+            args.result
+        )
+        mainViewModel.insertFavoriteRecipe(favoriteEntity)
+
+        changeMenuItemColor(item, R.color.yellow)
+        showSnackBar("Recipe saved.")
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(
+            details_layout,
+            message,
+            Snackbar.LENGTH_SHORT
+        ).setAction("Okay") {}
+            .show()
+    }
+
+    private fun changeMenuItemColor(item: MenuItem, color: Int) {
+        item.icon?.setTint(
+            ContextCompat.getColor(
+                this, color
+            )
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.details_menu, menu)
 
         var drawable = menu?.findItem(R.id.save_to_favorite_menu)?.icon
-        if(drawable != null){
-          drawable = DrawableCompat.wrap(drawable)
+        if (drawable != null) {
+            drawable = DrawableCompat.wrap(drawable)
             DrawableCompat.setTint(drawable, ContextCompat.getColor(this, R.color.white))
             menu?.findItem(R.id.save_to_favorite_menu)?.setIcon(drawable)
         }
