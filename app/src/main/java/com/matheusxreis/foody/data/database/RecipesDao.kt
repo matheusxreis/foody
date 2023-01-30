a70251cc1b731d90ce534913415dd34ac7c92b9a
Matheus Reis <matheus.reis081@gmail.com>
2023-01-30 20:06:55 -0300

    feat: added food joke table functions in dao

--- app/src/main/java/com/matheusxreis/foody/data/database/RecipesDao.kt
+++ app/src/main/java/com/matheusxreis/foody/data/database/RecipesDao.kt
@@ -3,6 +3,7 @@ package com.matheusxreis.foody.data.database
 import androidx.room.*
 import com.matheusxreis.foody.data.database.entities.FavoritesEntity
 import com.matheusxreis.foody.data.database.entities.RecipesEntity
+import com.matheusxreis.foody.models.FoodJoke
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -15,11 +16,17 @@ interface RecipesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFoodJoke(foodJoke: FoodJoke)
+
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
 
     @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
     fun readFavoriteRecipes():Flow<List<FavoritesEntity>>
+
+    @Query("SELECT * FROM food_joke_table ORDER BY id ASC")
+    fun readFoodJoke(): Flow<List<FoodJoke>>
 
     @Delete
     suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
