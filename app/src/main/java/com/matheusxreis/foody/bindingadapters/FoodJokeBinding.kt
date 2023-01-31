275bbce54dd827eb6b77f6a82df9f2f790af401e
Matheus Reis <matheus.reis081@gmail.com>
2023-01-30 22:27:41 -0300

    feat: created food joke binding adapter to define visibility

---
+++ app/src/main/java/com/matheusxreis/foody/bindingadapters/FoodJokeBinding.kt
@@ -0,0 +1,66 @@
+package com.matheusxreis.foody.bindingadapters
+
+import android.view.View
+import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
+import com.google.android.material.card.MaterialCardView
+import com.matheusxreis.foody.data.database.entities.FoodJokeEntity
+import com.matheusxreis.foody.models.FoodJoke
+import com.matheusxreis.foody.utils.NetworkResult
+
+class FoodJokeBinding {
+    companion object {
+
+        @BindingAdapter(
+            "readApiResponse3",
+            "readDatabase3",
+            requireAll = false
+        )
+        @JvmStatic
+        fun setCardAndProgressVisibility(
+            view: View,
+            apiResponse: NetworkResult<FoodJoke>?,
+            database: List<FoodJokeEntity>?
+        ){
+            when(apiResponse) {
+                is NetworkResult.Loading -> {
+                    when (view) {
+                        is ProgressBar -> {
+                            view.visibility = View.VISIBLE
+                        }
+                        is MaterialCardView -> {
+                            view.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                is NetworkResult.Error -> {
+                    when (view) {
+                        is ProgressBar -> {
+                            view.visibility = View.INVISIBLE
+                        }
+                        is MaterialCardView -> {
+                            view.visibility = View.VISIBLE
+                            if (database != null) {
+                                if (database.isEmpty()) {
+                                    view.visibility = View.INVISIBLE
+                                }
+                            }
+                        }
+                    }
+                }
+                is NetworkResult.Success -> {
+                    when (view) {
+                        is ProgressBar -> {
+                            view.visibility = View.INVISIBLE
+                        }
+                        is MaterialCardView -> {
+                            view.visibility = View.VISIBLE
+                        }
+                    }
+                }
+                null -> {}
+            }
+
+        }
+    }
+}
